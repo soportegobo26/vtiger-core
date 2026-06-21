f4346c45c10fda45dc32f7a1a1972344efa29f54
soportegobo26 <soportegobo26@github.com>
2026-06-20 20:01:46 -0500

    Fix multi-platform build support for clients

--- vtiger-core/vtiger-source/config_override.dev.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/** Logging configuration for Strict Development */
-require_once "vtlib/Vtiger/Utils/PhpLogHandler.php";
-Vtiger_PhpLogHandler::enableStrictLogging(__DIR__, "logs/phperr.log");
--- vtiger-core/vtiger-source/libraries/adodb_vtigerfix/VTIGERFIX.md
+++
@@ -1,6 +0,0 @@
-Library version: v5.21.2
-
-Fix to add custom constraint and changes to table.
-
-* adodb-xmlschema.inc.php
-* adodb-datadict.inc.php
